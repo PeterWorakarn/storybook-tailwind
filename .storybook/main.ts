f69750d038de03cc44bfbeadb5f1e1a6bbee016f
Worakarn Oun <skboardgames@gmail.com>
2021-11-13 13:10:27 +0700

    Remove fail dependency

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,6 @@ module.exports = {
   ],
   addons: [
     '@storybook/addon-actions/register',
-    '@storybook/addon-knobs/register',
     '@storybook/addon-notes/register',
   ],
   webpackFinal: async config => {
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,14 +2,13 @@
 import React from "react";
 import { Button } from '../components/Button';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, text, boolean } from "@storybook/addon-knobs";
 
 export default {
   title: "Button",
 };
 
 export const primary = () => {
-  const label = text("Label", "See now");
+  const label = "See now";
   return (
     <Button onClick={action('clicked')} label={label} />
   )
