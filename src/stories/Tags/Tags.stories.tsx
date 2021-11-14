eec146f425cf73355f4b0c128594956845655b41
Worakarn Oun <skboardgames@gmail.com>
2021-11-15 00:47:50 +0700

    Tags Component

---
+++ src/stories/Tags/Tags.stories.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import { withKnobs, text } from "@storybook/addon-knobs";
+import { Tags } from "./Tags";
+import { HiHashtag } from "react-icons/hi";
+import { action } from "@storybook/addon-actions";
+
+
+export default {
+  title: "Component/Tags",
+  component: Tags,
+  decorators: [
+    withKnobs
+  ],
+};
+
+export const Tag = () => {
+  const items = [
+    {
+      title: 'AAA'
+    },
+    {
+      title: 'BBB'
+    },
+    {
+      title: 'CCC'
+    },
+  ]
+  return (
+    <Tags items={items} onSelect={action('clicked')} />
+  )
+};
+export const TagWithIcon = () => {
+  const items = [
+    {
+      icon: <HiHashtag />,
+      title: 'AAA'
+    },
+    {
+      icon: <HiHashtag />,
+      title: 'BBB'
+    },
+    {
+      icon: <HiHashtag />,
+      title: 'CCC'
+    },
+  ]
+  return (
+    <Tags items={items} onSelect={action('clicked')} />
+  )
+};
---
+++ src/stories/Tags/Tags.tsx
@@ -0,0 +1,54 @@
+import React, { FC, useState } from 'react';
+import '../../base.css';
+
+type TagsTypes = {
+  items: {
+    icon?: React.ReactElement,
+    title: string,
+  }[];
+  onSelect: (value: string) => void;
+}
+
+export const Tags: FC<TagsTypes> = (props) => {
+  const [current, setCurrent] = useState('');
+  const onSelectHandler = (value: string) => {
+    setCurrent(value);
+    props.onSelect(value);
+  }
+
+  return (
+    <div className="flex justify-start flex-wrap gap-2 mt-2">
+      {props.items.map((tag: {
+        icon?: React.ReactElement<any, string | React.JSXElementConstructor<any>> | undefined;
+        title: string;
+      }) => {
+        return (
+          <label htmlFor={tag.title} key={tag.title}>
+            {current === tag.title ?
+              <input
+                type="radio"
+                className="checkboxFilter sr-only"
+                id={tag.title}
+                checked
+                name="tags"
+                value={tag.title}
+                onChange={() => onSelectHandler(tag.title)}
+              /> :
+              <input
+                type="radio"
+                className="checkboxFilter sr-only"
+                id={tag.title}
+                name="tags"
+                value={tag.title}
+                onChange={() => onSelectHandler(tag.title)}
+              />}
+
+            <div className="chip flex gap-1 justify-center items-center transition-colors border-2 dark:border-none border-gray-100 cursor-pointer text-gray-500 dark:text-typo-minor dark:border-typo-minor py-0.5 px-4 rounded-md">
+              {tag.icon}{tag.title}
+            </div>
+          </label>
+        );
+      })}
+    </div>
+  );
+};
