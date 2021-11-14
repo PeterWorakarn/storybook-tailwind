d866c6869b48e66cf4faa4437076c337e5ba5b0a
Worakarn Oun <skboardgames@gmail.com>
2021-11-14 23:44:48 +0700

    FAB

---
+++ src/stories/FAB/FAB.stories.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import { Meta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean } from "@storybook/addon-knobs";
+import { HiHashtag } from 'react-icons/hi';
+import { FAB } from "./FAB";
+import { PositionType } from "../../constant-enum-types";
+
+
+export default {
+  title: "Floating Action Button",
+  component: FAB,
+  decorators: [
+    withKnobs
+  ],
+};
+
+export const TopRight = () => {
+  const label = text("Label", "FAB");
+  return (
+    <FAB position={PositionType.TOP_RIGHT} label={label} icon={<HiHashtag />} onClick={action('clicked')} />
+  )
+};
+export const TopLeft = () => {
+  const label = text("Label", "FAB");
+  return (
+    <FAB position={PositionType.TOP_LEFT} label={label} icon={<HiHashtag />} onClick={action('clicked')} />
+  )
+};
+export const BottomRight = () => {
+  const label = text("Label", "FAB");
+  return (
+    <FAB position={PositionType.BOTTOM_RIGHT} label={label} icon={<HiHashtag />} onClick={action('clicked')} />
+  )
+};
+export const BottomLeft = () => {
+  const label = text("Label", "FAB");
+  return (
+    <FAB position={PositionType.BOTTOM_LEFT} label={label} icon={<HiHashtag />} onClick={action('clicked')} />
+  )
+};
---
+++ src/stories/FAB/FAB.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react';
+import { PositionType } from '../../constant-enum-types';
+import '../../base.css'
+
+type FABTypes = {
+  label: string;
+  position: PositionType;
+  icon: React.ReactElement;
+  onClick(): void;
+}
+
+export const FAB:FC<FABTypes> = (props) => {
+  const position = (pos: PositionType) => {
+    const key = pos;
+    switch (key) {
+      case PositionType.BOTTOM_LEFT:
+        return 'bottom-2 left-2';
+      case PositionType.BOTTOM_RIGHT:
+        return 'bottom-2 right-2';
+      case PositionType.TOP_LEFT:
+        return 'top-2 left-2';
+      case PositionType.TOP_RIGHT:
+        return 'top-2 right-2';
+      default:
+        break;
+    }
+  }
+  return (
+    <button id={props.label} type="button" onClick={props.onClick} className={`${position(props.position)} fixed z-50 bg-opacity-75 grid place-items-center w-8 h-8 bg-white rounded-full hover:bg-white active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none`}>
+      {props.icon && props.icon}
+      <span className="sr-only">{props.label}</span>
+    </button>
+  );
+};
