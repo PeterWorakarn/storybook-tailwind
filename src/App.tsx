02cadca731407fb5adaf34824809ebbf30e38ae5
Worakarn Oun <skboardgames@gmail.com>
2021-11-14 23:44:14 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from './components/Button';
+import { Button } from './stories/Button';
 import logo from './logo.svg';
 import './App.css';
 import './tailwind.css';
