37902cdb3292dd173c275205d61e2028ded9cd36
Worakarn Oun <skboardgames@gmail.com>
2021-12-13 01:16:11 +0700

    [Feature] Implement Dropdown

---
+++ src/stories/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React, { useState } from "react";
+import { Dropdown } from "./Dropdown";
+import { action } from "@storybook/addon-actions";
+
+
+export default {
+  title: "Component/Dropdown",
+  component: Dropdown,
+};
+
+export const PlainDropdown = () => {
+  const [value, setValue] = useState('');
+  const items = [
+    'item 1',
+    'item 2',
+    'item 3',
+  ]
+  return (
+    <Dropdown
+      name="dropdown"
+      items={items}
+      selectItem={value}
+      onSelect={action('Clicked')}
+      onChangeHandler={(key) => setValue(key)}
+    />
+  )
+};
--- src/stories/components/Dropdown/Dropdown.tsx
+++ src/stories/components/Dropdown/Dropdown.tsx
@@ -1,19 +1,57 @@
 import React, { FC } from 'react';
+import { useState } from 'react';
 import '../../../base.css'
 
 type DropdownTypes = {
-    label: string;
+    name: string;
+    selectItem: string;
+    items: string[];
+    onSelect: (value: string) => void;
+    onChangeHandler: (value: string) => void;
 }
 
-export const Header: FC<DropdownTypes> = (props) => {
-    const { label, children, ...rest } = props;
+export const Dropdown: FC<DropdownTypes> = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onSetValue = (value: string) => {
+        props.onSelect(value);
+        props.onChangeHandler(value);
+    }
 
     return (
-        <div className="flex gap-0">
-            <div className="h-8 w-1 rounded-full primary-red-gradient" />
-            <h2 className="uppercase font-semibold text-2xl px-4" {...rest}>
-                {label ? label : 'This is Header'}
-            </h2>
+        <div className="dropdown__container">
+            <button onClick={() => setIsOpen((prev) => !prev)} type="button" className="dropdown__input" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                <input
+                    className="dropdown__input__field"
+                    type="text"
+                    id={props.name}
+                    name={props.name}
+                    value={props.selectItem}
+                />
+                    <svg className="dropdown__input__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                    </svg>
+            </button>
+            <div
+                onClick={() => setIsOpen(false)}
+                className={`dropdown__backdrop ${isOpen ? 'opacity-100' : 'invisible opacity-0 hidden'} transition-all`}
+            >
+                <div
+                    className="dropdown__items"
+                    role="menu"
+                    aria-orientation="vertical"
+                    tabIndex={0}
+                    aria-labelledby="menu-button">
+                    <div
+                        className="py-1"
+                        role="none"
+                    >
+                        {props.items.map(item => (
+                            <span key={item} onClick={() => { onSetValue(item) }} className="dropdown__item text-center hover:text-primary-red cursor-pointer text-gray-700 block px-4 py-2 text-sm" role="menuitem">{item}</span>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
